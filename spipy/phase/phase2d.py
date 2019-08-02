a5cb5f584587b01bf511055d77dc5c4d3bd9fb85
YC.S <shiyc1994@gmail.com>
2019-08-02 15:11:34 +0800

    update phasing
    for stable cluster submission

--- spipy/phase/phase2d.py
+++ spipy/phase/phase2d.py
@@ -165,17 +165,18 @@ def run(num_proc=1, nohup=False, cluster=False):
 	subprocess.check_call(cmd, shell=True)
 
 	if num_proc >= 1:
+		# python path
+		pythony = subprocess.check_output('which python', shell=True).strip("\n")
 		# mpirun path
-		mpirun = subprocess.check_output('which python', shell=True).split('bin')[0]
+		mpirun = pythony.split('bin')[0]
 		mpirun = os.path.join(mpirun, 'bin/mpirun')
 		if nohup == True:
-			cmd = mpirun + " -n "+str(num_proc)+" python " + os.path.join(code_path, 'phase.py') + ' ' + os.path.join(_workpath, 'input.h5') + ' &>' + os.path.join(_workpath, 'phase.log') + '&'
+			cmd = mpirun + " -n "+str(num_proc)+" %s " % pythony + os.path.join(code_path, 'phase.py') + ' ' + os.path.join(_workpath, 'input.h5') + ' &>' + os.path.join(_workpath, 'phase.log') + '&'
 		else:
-			cmd = mpirun + " -n "+str(num_proc)+" python " + os.path.join(code_path, 'phase.py') + ' ' + os.path.join(_workpath, 'input.h5')
+			cmd = mpirun + " -n "+str(num_proc)+" %s " % pythony + os.path.join(code_path, 'phase.py') + ' ' + os.path.join(_workpath, 'input.h5')
 		if cluster:
 			print("\n Dry run on cluster, check submit_job.sh for details.\n")
 			submitfile = open(os.path.join(_workpath, "submit_job.sh"), 'w')
-			submitfile.write("#! /bin/bash\n\n")
 			submitfile.write("# Submit the command below to your job submitting system to run 3d phasing\n")
 			submitfile.write(cmd + '\n')
 			submitfile.close()
